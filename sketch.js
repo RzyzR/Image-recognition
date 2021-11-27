615bd0b71fce66f320dcd77d8efa5704392a9037
RzyzR <52755789+RzyzR@users.noreply.github.com>
2021-11-26 22:57:50 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,3 @@
-// Creado ChepeCarlos de ALSW
-// Tutorial Completo en https://nocheprogramacion.com
-// Canal Youtube https://youtube.com/alswnet?sub_confirmation=1
-
 var Camara;
 var RelacionCamara;
 var CartaMensaje;
